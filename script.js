8acbbc2b3c4cf3716934355593adb5a4994681d7
SashaWaszaj <166536612+SashaWaszaj@users.noreply.github.com>
2024-05-28 09:55:28 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+function pizzaOven(tipoCorteza, salsa, quesos, toppings) {
+    var pizza = {};
+    pizza.tipoCorteza = tipoCorteza;
+    pizza.salsa = salsa;
+    pizza.quesos = quesos;
+    pizza.toppings = toppings;
+    return pizza;
+}
+    
+var s1 = pizzaOven("estiloChicago", "marinara", "mozzarella", ["peperonni", "salchicha"]);
+console.log(s1);
+
+function pizzaOven(tipoCorteza, salsa, quesos, toppings) {
+    var pizza = {};
+    pizza.tipoCorteza = tipoCorteza;
+    pizza.salsa = salsa;
+    pizza.quesos = quesos;
+    pizza.toppings = toppings;
+    return pizza;
+}
+    
+var s2 = pizzaOven("lanzadaAMano", "marinara", ["mozzarella", "feta"], ["champiniones", "aceitunas", "cebollas"]);
+console.log(s2);
+
+function pizzaOven(tipoCorteza, salsa, quesos, toppings) {
+    var pizza = {};
+    pizza.tipoCorteza = tipoCorteza;
+    pizza.salsa = salsa;
+    pizza.quesos = quesos;
+    pizza.toppings = toppings;
+    return pizza;
+}
+    
+var s3 = pizzaOven("crocante", "deLaCasa", ["mozzarella", "cheddar"], ["bacon", "cebolla", "carne"]);
+console.log(s3);
+
+function pizzaOven(tipoCorteza, salsa, quesos, toppings) {
+    var pizza = {};
+    pizza.tipoCorteza = tipoCorteza;
+    pizza.salsa = salsa;
+    pizza.quesos = quesos;
+    pizza.toppings = toppings;
+    return pizza;
+}
+    
+var s4 = pizzaOven("bordeRelleno", "Barbacoa", ["mozzarella", "roquefort"], ["tomate", "rucula", "jamonCrudo"]);
+console.log(s4);
